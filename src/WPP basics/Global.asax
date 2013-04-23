a38d1c59aa4b8262e50671b0bba091eda98ed74f
Artur Dorochowicz <artur@dorochowicz.com>
2013-04-23 22:44:57 +0200

    Web Application project with WPP basic properties.

---
+++ src/WPP basics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WPP_defaults.MvcApplication" Language="C#" %>
---
+++ src/WPP basics/default.ps1
@@ -0,0 +1,23 @@
+task defaults {
+    msbuild "WPP basics.csproj" /verbosity:minimal /property:RequireRestoreConsent=false /property:Configuration=Release `
+        /property:DeployOnBuild=true
+}
+
+task basic_properties {
+    msbuild "WPP basics.csproj" /verbosity:minimal /property:RequireRestoreConsent=false /property:Configuration=Release `
+        /property:DeployOnBuild=true `
+        /property:DeployTarget=Package `
+        /property:AutoParameterizationWebConfigConnectionStrings=false `
+        /property:EnablePackageProcessLoggingAndAssert=true `
+        /property:FilesToIncludeForPublish=OnlyFilesToRunTheApp `
+        /property:WebPublishPipelineProjectName=MyWebApp `
+        '/property:DefaultDeployIisAppPath=Default Web Site\MyWebApp123'
+
+# Defaults:
+#  DeployTarget=Package
+#  AutoParameterizationWebConfigConnectionStrings=true
+#  EnablePackageProcessLogginAndAssert=false
+#  FilesToIncludeForPublish=OnlyFilesToRunTheApp
+#  WebPublishPipelineProjectName=<project-name>
+#  DefaultDeployIisAppPath=Default Web Site\<project-name>
+}
