bc9c8cbfccbca46930cb7dd698d9db7fa11ad01b
Artur Dorochowicz <artur.dorochowicz@holte.no>
2013-05-07 18:36:43 +0200

    Build time value/defaultValue on a parameter.

--- src/parameters/default.ps1
+++ src/parameters/default.ps1
@@ -1,4 +1,5 @@
 task package {
     msbuild 'parameters.csproj' /verbosity:minimal /property:RequireRestoreConsent=false /property:Configuration=Release `
-        /property:DeployOnBuild=true
+        /property:DeployOnBuild=true `
+        /property:MyOtherSettingParameterDefaultValue=def
 }
